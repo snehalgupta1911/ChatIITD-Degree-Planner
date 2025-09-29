ca88fdbe70b02211b63653d9562767946379e64d
Snehal Gupta <snehalguptaiitd@gmail.com>
2025-09-29 23:21:52 +0530

    Updated requirements.txt

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,22 +1,9 @@
-for course in courses_left[sem]:
-        course_data = None
-        for c in courses_left[sem]:
-            if c["code"] == code:
-                course_data = c
-                break
-        if course_data is not  None and course_data.get("prereqs"):
-             for prereq_code in course_data["prereqs"]:
-                prereq_found = False
-                for sem_p, courses in courses_left.items():
-                    for c in courses:
-                        if c["code"] == prereq_code:
-                            prereq_var = course_vars[(sem_p, prereq_code)]
-                            prereq_found = True
-                            # Add constraint: if we take this course, we must take the prereq
-                            model.Add(var <= prereq_var)
-                            # Add constraint: prereq must happen in an earlier semester
-                            model.Add(sem_p * prereq_var < sem * var)
-                            break
-                if prereq_found:
-                    break
-                 
+course_vars = {}
+# creation of boolean variables for all courses_sem Eg: ELL202_sem3 - yes or no 
+# we create a dictionary course_var in which we add tuple:bool var as key:value pair e.g. (3, "ELL202"): BoolVar("ELL202_sem3"),
+
+
+for course_code in set(c["code"] for sem_courses in courses_left.values() for c in sem_courses):
+    # For each uncompleted course, allow it to be scheduled in any remaining semester
+    for sem in range(user1.current_semester, user1.num_semesters + 1):
+        course_vars[(sem, course_code)] = model.NewBoolVar(f"{course_code}_sem{sem}")
